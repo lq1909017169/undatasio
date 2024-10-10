36676d27dd268ca4122416daed1caa5fc9d9d1e6
ForgetYou <staydxj9933@163.com>
2024-10-10 10:45:58 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='undatasio',                      # 包名
       author='lily',
       author_email='1909017169@qq.com',
       url='',
-      install_requires=["requests~=2.32.3", "requests_toolbelt==1.0.0"],	                # 依赖包会同时被安装
+      install_requires=["requests~=2.32.3", "requests_toolbelt==1.0.0", "llama_index", "langchain_core"],	                # 依赖包会同时被安装
       license='MIT',
       python_requires=">=3.8",  # 项目依赖的 Python 版本
       packages=find_packages())
