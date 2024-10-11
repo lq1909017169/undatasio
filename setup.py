a9662a42e3af7f1abca96810e49009d8e099c4d2
ForgetYou <staydxj9933@163.com>
2024-10-11 15:58:29 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import pathlib
 from distutils.core import setup
 from setuptools import find_packages
+import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
@@ -9,6 +9,7 @@ setup(name='undatasio',                      # 包名
       version='0.1.7',                      # 版本号
       description='test pip upload',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='lily',
       author_email='1909017169@qq.com',
       url='',
