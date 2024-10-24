3e80d6e5446a9d37049f8eebee0abbac21ea7e08
ForgetYou <staydxj9933@163.com>
2024-10-24 18:38:08 +0800

    upload setup

--- setup.py
+++ setup.py
@@ -1,19 +1,51 @@
 from distutils.core import setup
 from setuptools import find_packages
 import pathlib
+import re
+import os
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setup(name='undatasio',  # 包名
-      version='0.1.7.2',  # 版本号
+package = 'undatasio'
+
+
+def get_version():
+    """
+    从包的 __init__.py 文件中提取版本号.
+
+    参数:
+      package: 包的名称.
+
+    返回:
+      版本号字符串, 如果找不到则返回 '0.0.0'.
+    """
+    init_py = os.path.join(package, '__init__.py')
+    with open(init_py, 'r') as f:
+        content = f.read()
+
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if version_match:
+        return version_match.group(1)
+    else:
+        raise RuntimeError(f"无法在 '{init_py}' 中找到版本号")
+
+
+setup(name=package,  # 包名
+      version=get_version(),  # 版本号
       description='python sdk for the undatasio platform',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='1909017169',
       author_email='1909017169@qq.com',
       url='',
-      install_requires=["requests~=2.32.3", "requests_toolbelt==1.0.0", "llama_index", "langchain_core"],  # 依赖包会同时被安装
+      install_requires=[
+          "requests~=2.32.3",
+          "requests_toolbelt==1.0.0",
+          "llama_index",
+          "langchain_core",
+          "pandas"
+      ],  # 依赖包会同时被安装
       license='MIT',
       python_requires=">=3.10",  # 项目依赖的 Python 版本
       packages=find_packages()
--- undatasio/__init__.py
+++ undatasio/__init__.py
@@ -1,1 +1,3 @@
 from .undatasio import *
+
+__version__ = '0.1.7.4'
